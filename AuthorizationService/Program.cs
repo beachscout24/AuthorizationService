fd710069ed3c0b7050c8463efe226214b0722b24
beachscout24 <eric.w.clarke@gmail.com>
2024-07-19 16:43:04 -0400

    Modified Program.cs removed unused usings

--- AuthorizationService/Program.cs
+++ AuthorizationService/Program.cs
@@ -1,11 +1,6 @@
-using AuthorizationService.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Configuration;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
